545111938e03660b827337711ca58285b6bafa7b
Cameron <vandervalk.cameron@gmail.com>
2022-07-07 13:36:13 +0200

    contact area loading state to succes message working. alert if failure occurs

--- app.js
+++ app.js
@@ -4,20 +4,25 @@
 
 function contact(event) {
     event.preventDefault();
-    // emailjs
-    // .sendForm (
-    //     'service_qlkhxiq',
-    //     'template_48ylnpe',
-    //     event.target,
-    //     'Q_RyNeu2jmf94M_0x'
-    // ).then (() => {
-    //     console.log('this worked1')
-    // });
-    const loading = document.querySelector('.modal__overlay--loading')
-    const success = document.querySelector('.modal__overlay--success')
-    loading.classList += ' modal__overlay--visible' 
-    setTimeout(() => {
-        console.log('it worked 1')
-    }, 500);
+    const loading = document.querySelector('.modal__overlay--loading');
+    const success = document.querySelector('.modal__overlay--success');
+    loading.classList += ' modal__overlay--visible';
+    emailjs
+        .sendForm(
+            'service_qlkhxiq',
+            'template_48ylnpe',
+            event.target,
+            'Q_RyNeu2jmf94M_0x'
+        ).then(() => {
+            loading.classList.remove('modal__overlay--visible');
+            success.classList += ' modal__overlay--visible';
+        }).catch(() => {
+            loading.classList.remove('modal__overlay--visible');
+            alert(
+                'the email service is temporarily unavailable. Please contact me directly on email - vandervalk.cameron@gmail.com'
+            );
+
+        });
+
 };
 
