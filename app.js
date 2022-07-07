28071ab66922af9f0158656b29aa411da0ed6b43
Cameron <vandervalk.cameron@gmail.com>
2022-07-07 09:41:26 +0200

    loading state function

--- app.js
+++ app.js
@@ -2,7 +2,22 @@
 // service_qlkhxiq
 // Q_RyNeu2jmf94M_0x
 
-function contact (event) {
+function contact(event) {
     event.preventDefault();
-    console.log('it worked')
-}
+    // emailjs
+    // .sendForm (
+    //     'service_qlkhxiq',
+    //     'template_48ylnpe',
+    //     event.target,
+    //     'Q_RyNeu2jmf94M_0x'
+    // ).then (() => {
+    //     console.log('this worked1')
+    // });
+    const loading = document.querySelector('.modal__overlay--loading')
+    const success = document.querySelector('.modal__overlay--success')
+    loading.classList += ' modal__overlay--visible' 
+    setTimeout(() => {
+        console.log('it worked 1')
+    }, 500);
+};
+
