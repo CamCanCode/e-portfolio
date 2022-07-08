d308859e818de415923de736ce8fb41a1b997eed
Cameron <vandervalk.cameron@gmail.com>
2022-07-08 14:04:10 +0200

    dark theme added on click of contrast button in nav bar

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
+let contrastToggle = false;
 
+function toggleContrast() {
+    contrastToggle = !contrastToggle;
+    if (contrastToggle) {
+      document.body.classList += " dark-theme"
+    }
+    else {
+      document.body.classList.remove("dark-theme")
+    }
+  }
 
 function contact(event) {
     event.preventDefault();
